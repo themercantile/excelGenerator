ead0f22fa97f8993a2d2bbbadc04a2d9dda39202
themercantile <mercantile.code@gmail.com>
2021-04-24 20:15:54 +0800

    Cleaned up the code and added bootstrap panels for the output.

--- myscript.js
+++ myscript.js
@@ -1,8 +1,7 @@
 /* This is the processing part of the Create Excel Formulas project.
 We're going to use jQuery as well to make things easier.
 Need to provide instructions in the first div section and then get them to input the desired stuff in the second div section. 
-headSection and inputSection are the IDs for the divs.
-=HYPERLINK(CONCATENATE("https://zoomies.pupper.org/", A1, "/displayme.jizz"), "CLICK")
+I'm using the following 2 lines as guides on the correct formats
 https://zoomies.pupper.org/MYRECOD122/displayme.jizz
 start chrome "https://zoomies.pupper.org/", A1, "/displayme.jizz"
 */
@@ -11,8 +10,9 @@ function createURL() {
   let fullURL = document.getElementById("fullURL").value;
   console.log(fullURL);
   let recordVar = document.getElementById("recordVar").value;
-  if (fullURL.search(recordVar)===-1) {
+  if (fullURL.search(recordVar)===-1) { // This line is searching for the string in the URL. If it isn't there a -1 is returned.
     document.getElementById("outURL").innerHTML = "ERROR!";
+    document.getElementById("batchURL").innerHTML = "ERROR!";
     return;
   }
   console.log(recordVar);
@@ -21,9 +21,6 @@ function createURL() {
   let newURL = "=HYPERLINK(CONCATENATE(\"" + fullURL.replace(recordVar, excelCell) + "\"), \"Open Hyperlink\")";
   console.log(newURL);
   let chromeURL = "=CONCATENATE(\"start chrome " + fullURL.replace(recordVar, excelCell) + "\")";
-  //start chrome https://zoomies.pupper.org/", A1, "/displayme.jizz
-  
-  //=CONCATENATE("start chrome https://zoomies.pupper.org/", A1, "/displayme.jizz"
   document.getElementById("outURL").innerHTML = newURL;
   document.getElementById("batchURL").innerHTML = chromeURL;
 }
